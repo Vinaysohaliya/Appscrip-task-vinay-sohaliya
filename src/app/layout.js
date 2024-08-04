75afabfc34b7349b574935b6a36fe0d45d036f93
Vinaysohaliya <sohaliyavin01@gmail.com>
2024-08-04 15:17:57 +0530

    SEO added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,36 @@
+// app/layout.js
 import './globals.css';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
-import { Poppins } from 'next/font/google'
+import Head from 'next/head';
 
-const poppins_init = Poppins({
-    weight: '400',
-    subsets: ['latin'],
-    variable:'--font-poppins',
-})
+export const metadata = {
+  title: 'ClickCart - Your Online Shopping Destination',
+  description: 'ClickCart offers a wide variety of products at unbeatable prices. Shop now and enjoy fast shipping and excellent customer service.',
+};
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en" className={poppins_init.className}>
-            <head>
-                <title>Page Title</title>
-                <meta name="description" content="Page Description" />
-            </head>
-            <body>
-                <Header />
-                <hr />
-                {children}
-                <Footer />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+        <script type="application/ld+json">
+          {JSON.stringify({
+            '@type': 'WebSite',
+            name: metadata.title,
+            description: metadata.description,
+            url: 'https://appscrip-task-vinay-sohaliya-k7g9.vercel.app/', 
+          })}
+        </script>
+      </Head>
+      <body>
+        <Header />
+        <hr />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
 }
--- src/components/Main/ProductList.js
+++ src/components/Main/ProductList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from '../../styles/Main/Main.module.css';
 import { Heart } from 'lucide-react';
+import Image from 'next/image';
 
 const ProductList = ({ products }) => {
     return (
         <section className={styles.products}>
             {products.map(product => (
                 <div key={product.id} className={styles.productCard}>
-                    <img src={product.image} alt={product.title} className={styles.productImage} />
+                    <Image src={product.image} width={1000}  height={1000} alt={product.title}  className={styles.productImage} />
                     <h4>{product.title}</h4>
                     <div className={styles.productCardBottom}>
                         <p>Sign in or Create an account to see pricing</p>
